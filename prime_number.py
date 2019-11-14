8f3e7d57b233ab9d4c44763c05455c7e1593ddbe
John Smith <wtelecom212@gmai.com>
2019-11-14 14:45:25 +0200

    Мелкий правки

--- prime_number.py
+++ prime_number.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify, request
-from random import randint, choice
+from flask import Flask, jsonify
+from random import choice
 
 app = Flask(__name__)
 
@@ -20,27 +20,9 @@ def index():
     return "PONG"
 
 
-@app.route('/number2')
-def number2():
-    return jsonify(randint(1,500))
-
-
 @app.route('/number')
 def number():
     return jsonify(choice(primelist))
-
-
-def shutdown_server():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running')
-    func()
-
-
-@app.route('/shutdown')
-def shutdown():
-    shutdown_server()
-    return 'Server shutting down'
 
 
 if __name__ == '__main__':
